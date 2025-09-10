7494d83e91db41c60557f9e31290a32f07e68903
Dipta-b <diptabanik0@gmail.com>
2025-09-10 11:54:01 +0600

    craeted constructor and called

---
+++ Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    // Constructor
+    public Car() {
+        System.out.println("Car created");
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car(); // Contructor is called here Car() eitai contructor called part. r left er Car
+                               // hocche type/class name and type of the variable.
+    }
+
+}
